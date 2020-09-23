a5803bfe14eefebd9b6ac11f5c16a02001ba43f3
chong <cc.chen@maxpr.com.cn>
2020-09-23 17:10:07 +0800

    modify the version of pip

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="mysql-stream",
     packages=find_packages(),
-    version='0.0.1',
+    version='0.0.2',
     description="simple and easy to use mysql client and ORM library",
     author="chenchong",
     author_email='boy3565@163.com',
