83ab381a2ae799e43a82bd94f780f25b0d7c56cf
chong <cc.chen@maxpr.com.cn>
2020-09-23 15:40:32 +0800

    add code at first time

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="mysqlstream",
+    name="mysql-stream",
     packages=find_packages(),
     version='0.0.1',
     description="simple and easy to use mysql client and ORM library",
